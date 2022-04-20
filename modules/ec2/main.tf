763d1d8a930115a40461236db2b585c065cb9d8c
Walter Choi <walter.choi@6317.io>
2022-04-20 19:32:49 +0800

    linked network card to ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -11,6 +11,10 @@ resource "aws_instance" "this" {
   count = var.machine_count
   ami = var.ami
   instance_type = var.instance_type
+  network_interface {
+    network_interface_id = aws_network_interface.this[count.index].id
+    device_index         = 0
+  }
   tags = {
     Name = join("-",[var.tag_name,count.index + 1])
   } 
